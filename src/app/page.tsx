5672764758d3733caceea157f043cb55887e31d6
Aygyun Yakub <49846915+achu94@users.noreply.github.com>
2025-02-17 22:21:47 +0100

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
       <div className="grid grid-rows-[20px_.5fr_20px] items-center justify-items-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <SwapContainer />
-          {/* { tradingPairs.map( pair => JSON.stringify(pair)) } */}
         </main>
       </div>
       {/* <Footer /> */}
